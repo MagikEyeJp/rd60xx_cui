a2bf27b89264ff67247dd855cccb5ee2671dce15
uribo <ur1b0.dev@gmail.com>
2023-02-02 00:25:08 +0900

    rd60xxの情報/出力を確認する関数を追加

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ for port in ports:
         port_path = port.device
         break
 
+def print_device_info(rd60xx):
+    reg = rd60xx._read_registers(0,4)
+    print(f'MODEL: {reg[0]/10},SN: {reg[1]<<16 | reg[2]},FW: {reg[3]/100}')
+
+def print_device_status(rd60xx):
+    reg = rd60xx._read_registers(8,14)
+    print(f"OUT - V:{reg[2]/rd60xx.voltres},I:{reg[3]/rd60xx.ampres}")
+    print(f"SET - V:{reg[0]/rd60xx.voltres},I:{reg[1]/rd60xx.ampres}")
 
 if port_path is None:
     print("RD60XX is not connected.")
@@ -20,5 +28,5 @@ else:
 
 rd60xx = RD6006(port_path)
 
-print(rd60xx.status())
-
+print_device_info(rd60xx)
+print_device_status(rd60xx)
