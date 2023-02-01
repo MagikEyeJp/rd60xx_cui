7aac8f5ae13fc3fefb5152dba2970a6be0e30378
uribo <ur1b0.dev@gmail.com>
2023-02-01 22:44:54 +0900

    初期化処理を追加

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+from serial.tools import list_ports
+from rd6006 import RD6006
+
+ports = list_ports.comports()
+port_path = None
+
+for port in ports:
+    if "VID:PID=1A86:7523" in port.hwid:
+        port_path = port.device
+        break
+
+
+if port_path is None:
+    print("RD60XX is not connected.")
+    exit(1)
+else:
+    print(f"port_path:{port_path}")
+
+rd60xx = RD6006(port_path)
+
+print(rd60xx.status())
+
