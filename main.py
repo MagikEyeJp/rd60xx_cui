2c4743c6416defcc48b6511890ab6059d79fe7c2
uribo <ur1b0.dev@gmail.com>
2023-02-02 19:14:33 +0900

    クラスはやめた

--- main.py
+++ main.py
@@ -3,37 +3,37 @@
 from serial.tools import list_ports
 from rd6006 import RD6006
 
-ports = list_ports.comports()
-port_path = None
 
-for port in ports:
-    if "VID:PID=1A86:7523" in port.hwid:
-        port_path = port.device
-        break
-
-def print_device_info(rd60xx):
+def print_info(rd60xx):
     reg = rd60xx._read_registers(0,4)
     print(f'MODEL: {reg[0]/10}\tSN: {reg[1]<<16 | reg[2]}\tFW: {reg[3]/100}')
 
-def print_device_status(rd60xx):
+def print_status(rd60xx):
     reg = rd60xx._read_registers(8,14)
     print(f"[SETTING] {reg[0]/rd60xx.voltres}V\t{reg[1]/rd60xx.ampres}A")
     print(f"[CURRENT] {reg[2]/rd60xx.voltres}V\t{reg[3]/rd60xx.ampres}A")
-    print(f"[OUTPUT]  {'ON' if bool(reg[10]) else 'OFF'}\t{'CC' if bool(reg[9]) else 'CV'}")
+    print(f"[OUTPUT]  {'ON' if bool(reg[10]) else 'OFF'}")
 
-def print_separater():
+def separater():
     print("----------------------------------------")
 
-if port_path is None:
-    print("RD60XX is not connected.")
-    exit(1)
-else:
-    print(f"port_path:{port_path}")
+if __name__ == "__main__":
+    ports = list_ports.comports()
+    rd60xx = None
+    for port in ports:
+        if "VID:PID=1A86:7523" in port.hwid:
+            print(f"port_path:{port.device}")
+            rd60xx = RD6006(port.device)
+            break
 
-rd60xx = RD6006(port_path)
+    if rd60xx is None:
+        print("RD60XX not found.")
+        exit(1)
 
-print_separater()
-print_device_info(rd60xx)
-print_separater()
-print_device_status(rd60xx)
-print_separater()
+    separater()
+    print_info(rd60xx)
+    separater()
+    print_status(rd60xx)
+    separater()
+
+
